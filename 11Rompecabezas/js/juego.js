e54473de7fffbea7eb831817a9e5322835ce5edc
demonassit <jminorg@ipn.mx>
2023-03-08 10:04:42 -0600

    habia un error en el intercambio de posiciones

--- 11Rompecabezas/js/juego.js
+++ 11Rompecabezas/js/juego.js
@@ -1,6 +1,8 @@
 //vamos a crear un arreglo que contenga las instrucciones para el juego
 
-var instrucciones = ["Utilizar las flehcas para mover las piezas", "Ordenar las pierzas hasta alcanzar la imagen objetivo"];
+var instrucciones = ["Utilizar las flechas para mover las piezas",
+    "Ordenar las piezas hasta alcanzar la imagen objetivo"
+];
 
 //vamos a crear una variable para guardar los movimientos
 
@@ -13,7 +15,7 @@ var rompe = [
     [7, 8, 9]
 ];
 
-var rompeCorrecto = [
+var rompeCorrecta = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
@@ -25,15 +27,13 @@ var columnaVacia = 2;
 
 //necesito una funcion para recorrer el arreglo pasando por cada elemento 
 
-function mostrarInstrucciones(instrucciones){
-    for(var i = 0; i < instrucciones.length; i++){
-        alert(instrucciones);
-        mostrarInstruccionesEnlista(instrucciones[i], "lista-instrucciones");
+function mostrarInstrucciones(instrucciones) {
+    for (var i = 0; i < instrucciones.length; i++) {
+        mostrarInstruccionEnLista(instrucciones[i], "lista-instrucciones");
     }
 }
 
-function mostrarInstruccionesEnlista(instruccion, idLista){
-    //del html
+function mostrarInstruccionEnLista(instruccion, idLista) {
     var ul = document.getElementById(idLista);
     var li = document.createElement("li");
     li.textContent = instruccion;
@@ -41,30 +41,31 @@ function mostrarInstruccionesEnlista(instruccion, idLista){
 }
 
 //vamos a hacer una funcion para agregar la ultima direccion del movimiento
-function agregarUltimoMovimiento(direccion){
+function agregarUltimoMovimiento(direccion) {
     movimientos.push(direccion);
 }
 
 //una funcion para checar si gane
-function checarSiGano(){
-    for(var i = 0; i < rompe.length; i++){
-        for(var j = 0; j < rompe[i].length; j++){
-            //que i = ij
+function checarSiGano() {
+    for (var i = 0; i < rompe.length; i++) {
+        for (var j = 0; j < rompe[i].length; j++) {
             var rompeActual = rompe[i][j];
-            if(rompeActual != rompeCorrecto[i][j]){
+            if (rompeActual !== rompeCorrecta[i][j]) {
                 return false;
             }
         }
     }
+
     return true;
 }
 
 //funcion para decir que gane
-function mostrarCartelGanador(){
-    if(checarSiGano()){
-        alert("Wiiiii a mimir");
+function mostrarCartelGanador() {
+    if (chequearSiGano()) {
+        alert("Wiiii mimir");
     }
     return false;
+
 }
 
 //funcion para intercambiar las dos posiciones de la pieza 
@@ -73,22 +74,23 @@ arreglo[0][0] = arreglo[1][2];*/
 
 //intercambiar los valores por las posciiones
 
-function intercambiarPosicionesRompe(filaPos1, columnaPos1, filaPos2, columaPos2){
+function intercambiarPosicionesRompe(filaPos1, columnaPos1, filaPos2, columnaPos2) {
+
     var pos1 = rompe[filaPos1][columnaPos1];
-    var pos2 = rompe[filaPos2][columaPos2];
+    var pos2 = rompe[filaPos2][columnaPos2];
 
     rompe[filaPos1][columnaPos1] = pos2;
-    rompe[filaPos2][columaPos2] = pos1;
+    rompe[filaPos2][columnaPos2] = pos1;
 }
 
-function actualizarPosicionVacia(nuevaFila, nuevaColumna){
+function actualizarPosicionVacia(nuevaFila, nuevaColumna) {
     filaVacia = nuevaFila;
     columnaVacia = nuevaColumna;
 }
 
 //tenemos que checar si la posicion dentro del rompecabezas es la correcta
-function posicionValida(fila, columna){
-    return(fila >= 0 && fila <= 2 && columna >= 0 && columna <= 2);
+function posicionValida(fila, columna) {
+    return (fila >= 0 && fila <= 2 && columna >= 0 && columna <= 2);
 }
 
 //ahora viene la parte del movimiento de las piezas derivado a que el hueco es el que se mueve a intercambia posicion con cada una es por ello que debemos saber representar el movimiento de las teclas
@@ -140,21 +142,22 @@ var codigosDireccion= {
     ABAJO: 40
 };
 
-function intercambiarPosiciones(fila1, columna1, fila2, columna2){
+function intercambiarPosiciones(fila1, columna1, fila2, columna2) {
+    // Intercambio posiciones en la grilla
     var pieza1 = rompe[fila1][columna1];
     var pieza2 = rompe[fila2][columna2];
 
-    //intercambiarlos
     intercambiarPosicionesRompe(fila1, columna1, fila2, columna2);
-    intercambiarPosiciones('pieza ' + pieza1, 'pieza ' + pieza2 );
+    intercambiarPosicionesDOM('pieza' + pieza1, 'pieza' + pieza2);
 
 }
 
 //tengo que representar esos movimeintos dentro del dom
 
-function intercarPosicionesDOM(idpieza1, idpieza2){
-    var elementoPieza1 = document.getElementById(idpieza1);
-    var elementoPieza2 = document.getElementById(idpieza2);
+function intercambiarPosicionesDOM(idPieza1, idPieza2) {
+    // Intercambio posiciones en el DOM
+    var elementoPieza1 = document.getElementById(idPieza1);
+    var elementoPieza2 = document.getElementById(idPieza2);
 
     var padre = elementoPieza1.parentNode;
 
@@ -163,9 +166,6 @@ function intercarPosicionesDOM(idpieza1, idpieza2){
 
     padre.replaceChild(clonElemento1, elementoPieza2);
     padre.replaceChild(clonElemento2, elementoPieza1);
-
-
-
 }
 
 //actualizar los movimientos
