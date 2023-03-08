188249a7a1c82d4f86aa455bf46d2055cc1850d9
demonassit <jminorg@ipn.mx>
2023-03-08 09:54:29 -0600

    aun esta mal tT_T

--- 11Rompecabezas/js/juego.js
+++ 11Rompecabezas/js/juego.js
@@ -27,6 +27,7 @@ var columnaVacia = 2;
 
 function mostrarInstrucciones(instrucciones){
     for(var i = 0; i < instrucciones.length; i++){
+        alert(instrucciones);
         mostrarInstruccionesEnlista(instrucciones[i], "lista-instrucciones");
     }
 }
@@ -67,8 +68,8 @@ function mostrarCartelGanador(){
 }
 
 //funcion para intercambiar las dos posiciones de la pieza 
-arreglo[1][2] = arreglo[0][0];
-arreglo[0][0] = arreglo[1][2];
+/*arreglo[1][2] = arreglo[0][0];
+arreglo[0][0] = arreglo[1][2];*/
 
 //intercambiar los valores por las posciiones
 
@@ -140,8 +141,106 @@ var codigosDireccion= {
 };
 
 function intercambiarPosiciones(fila1, columna1, fila2, columna2){
-    
+    var pieza1 = rompe[fila1][columna1];
+    var pieza2 = rompe[fila2][columna2];
+
+    //intercambiarlos
+    intercambiarPosicionesRompe(fila1, columna1, fila2, columna2);
+    intercambiarPosiciones('pieza ' + pieza1, 'pieza ' + pieza2 );
+
 }
+
+//tengo que representar esos movimeintos dentro del dom
+
+function intercarPosicionesDOM(idpieza1, idpieza2){
+    var elementoPieza1 = document.getElementById(idpieza1);
+    var elementoPieza2 = document.getElementById(idpieza2);
+
+    var padre = elementoPieza1.parentNode;
+
+    var clonElemento1 = elementoPieza1.cloneNode(true);
+    var clonElemento2 = elementoPieza2.cloneNode(true);
+
+    padre.replaceChild(clonElemento1, elementoPieza2);
+    padre.replaceChild(clonElemento2, elementoPieza1);
+
+
+
+}
+
+//actualizar los movimientos
+
+function actualizarUltimoMovimiento(direccion){
+    var ultimoMov = document.getElementById('flecha');
+    switch(direccion){
+        case codigosDireccion.ARRIBA:
+            ultimoMov.textContent = '↑';
+            break;
+        case codigosDireccion.ABAJO:
+            ultimoMov.textContent = '↓';
+            break;
+        case codigosDireccion.DERECHA:
+            ultimoMov.textContent = '→';
+            break;
+        case codigosDireccion.IZQUIERDA:
+            ultimoMov.textContent = '←';
+            break;
+    }
+}
+
+
+
+
+function mezclarPiezas(veces){
+    if(veces <= 0){
+        alert("hola");
+        return;
+    }
+
+    var direcciones = [codigosDireccion.ABAJO, codigosDireccion.ARRIBA, codigosDireccion.DERECHA, codigosDireccion.IZQUIERDA];
+
+    var direccion = direcciones[Math.floor(Math.random() * direcciones.length)]; //barajeando
+
+    moverEnDireccion(direccion);
+
+    setTimeout(function() {
+        mezclarPiezas(veces - 1);
+    }, 100);
+}
+
+//vamos a capturar las teclas que este ingresando el jugador
+
+function capturarTeclas(){
+    //para saber que esta moviendo
+    document.body.onkeydown = (function(evento) {
+        if(evento.which === codigosDireccion.ABAJO || evento.which === codigosDireccion.ARRIBA || evento.which === codigosDireccion.DERECHA || evento.which === codigosDireccion.IZQUIERDA){
+            moverEnDireccion(evento.which);
+            actualizarUltimoMovimiento(evento.which);
+
+            var gano = checarSiGano();
+            if(gano){
+                setTimeout(function(){
+                    mostrarCartelGanador();
+                }, 500);
+            }
+            evento.preventDefault; 
+        }
+    } );
+}
+
+//que se incie el rompecabezas
+function iniciar(){
+    mezclarPiezas(30);
+    capturarTeclas();
+}
+
+//ejecutamos nuestro inicio
+
+iniciar();
+
+//mando a llamar a las instrucciones
+
+mostrarInstrucciones(instrucciones);
 
 
 
